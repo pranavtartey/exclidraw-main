ea7e713a8b531c51a0dcd41ecb4831172fa2d2c2
Pranav Tartey <pranavtartey@gmail.com>
2025-02-01 16:59:09 +0530

    added the common packages and exported the types form the packages

--- apps/http-backend/src/controllers/index.ts
+++ apps/http-backend/src/controllers/index.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "../config";
-
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types";
 
 export const Signup = (req: Request, res: Response) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Invalid inputs"
+        })
+        return
+    }
     //db-call
     res.json({
         userId: 123
@@ -11,6 +18,13 @@ export const Signup = (req: Request, res: Response) => {
 }
 
 export const Signin = (req: Request, res: Response) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Invalid inputs"
+        })
+        return
+    }
     const userId = 1;
     const token = jwt.sign({
         userId
@@ -19,6 +33,13 @@ export const Signin = (req: Request, res: Response) => {
     res.json(token)
 }
 export const Room = (req: Request, res: Response) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Invalid inputs"
+        })
+        return
+    }
     //db-call
     res.json({
         roomId: 123
