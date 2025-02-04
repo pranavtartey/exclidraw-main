ac1a43051df6ae653ae3399712a9fce3ff7a1e4a
Pranav Tartey <pranavtartey@gmail.com>
2025-02-05 03:05:41 +0530

    created a separate module for the canvas drawing logic

--- apps/frontend/app/canvas/[roomid]/page.tsx
+++ apps/frontend/app/canvas/[roomid]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Draw from "@/draw";
 import { FC, useEffect, useRef } from "react";
 
 const Canvas: FC = () => {
@@ -6,49 +7,12 @@ const Canvas: FC = () => {
 
   useEffect(() => {
     if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-
-      if (!ctx) {
-        return;
-      }
-      let clicked = false;
-      let startX = 0;
-      let startY = 0;
-
-      canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        startX = e.clientX;
-        startY = e.clientY;
-        // console.log(e.clientX);
-        // console.log(e.clientY);
-      });
-      canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-        // console.log(e.clientX);
-        // console.log(e.clientY);
-      });
-      canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-          const width = e.clientX - startX;
-          const height = e.clientY - startY;
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.strokeRect(startX, startY, width, height);
-
-          console.log(e.clientX);
-          console.log(e.clientY);
-        }
-      });
+      Draw(canvasRef.current);
     }
   }, [canvasRef]);
 
   return (
-    <canvas
-      className="bg-neutral-800"
-      ref={canvasRef}
-      width={700}
-      height={700}
-    ></canvas>
+    <canvas className="" ref={canvasRef} width={700} height={700}></canvas>
   );
 };
 
