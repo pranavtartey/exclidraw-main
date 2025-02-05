228ae89992611fab83a9dfe40d72b08289e2dfdd
Pranav Tartey <pranavtartey@gmail.com>
2025-02-05 17:22:38 +0530

    fixed the canvas clearing issue on mousse move

--- apps/frontend/draw/index.ts
+++ apps/frontend/draw/index.ts
@@ -1,5 +1,21 @@
+type Shape = {
+    type: "rect",
+    x: number,
+    y: number,
+    width: number,
+    height: number
+} | {
+    type: "circle",
+    centerX: number,
+    centerY: number,
+    radius: number
+}
+
+
 const Draw = (canvas: HTMLCanvasElement) => {
     const ctx = canvas.getContext("2d");
+
+    let existingShapes: Shape[] = []
 
     if (!ctx) {
         return;
@@ -19,6 +35,16 @@ const Draw = (canvas: HTMLCanvasElement) => {
     });
     canvas.addEventListener("mouseup", (e) => {
         clicked = false;
+        const width = e.clientX - startX
+        const height = e.clientY - startY
+        ClearCanvas(existingShapes, canvas, ctx)
+        existingShapes.push({
+            type: "rect",
+            height,
+            width,
+            x: startX,
+            y: startY
+        })
         // console.log(e.clientX);
         // console.log(e.clientY);
     });
@@ -29,6 +55,12 @@ const Draw = (canvas: HTMLCanvasElement) => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.fillStyle = "rgba(0,0,0)";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
+            existingShapes.map((shape) => {
+                if (shape.type === "rect") {
+                    ctx.strokeStyle = "rgba(255,255,255)"
+                    ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)
+                }
+            })
             ctx.strokeStyle = "rgba(255,255,255,255)";
             ctx.strokeRect(startX, startY, width, height);
 
@@ -36,6 +68,18 @@ const Draw = (canvas: HTMLCanvasElement) => {
             console.log(e.clientY);
         }
     });
+}
+
+const ClearCanvas = (existingShapes: Shape[], canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => {
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(0,0,0)";
+
+    existingShapes.map((shape) => {
+        if (shape.type === "rect") {
+            ctx.strokeStyle = "rgba(255,255,255)"
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)
+        }
+    })
 }
 
 export default Draw
