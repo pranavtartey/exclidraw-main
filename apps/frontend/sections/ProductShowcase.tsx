58faa2a456146407b6011d7425460586f1b8201f
Pranav Tartey <pranavtartey@gmail.com>
2025-02-13 15:56:54 +0530

    made some bug fixes

--- apps/frontend/sections/ProductShowcase.tsx
+++ apps/frontend/sections/ProductShowcase.tsx
@@ -15,11 +15,11 @@ const ProductShowcase = () => {
           </p>
         </div>
         <div className="border border-neutral-500 p-4 rounded-lg [mask-image:linear-gradient(to_bottom,black,transparent_80%)] relative">
-            <div className="absolute top-[30%] h-10 md:h-20 lg:h-36 w-20 md:w-40 lg:w-60 border border-neutral-400"/>
-            <div className="absolute top-[20%] left-[40%] h-20 md:h-40 lg:h-60 w-20 md:w-40 lg:w-60 rounded-full border border-neutral-400"/>
-            <div className="absolute top-[20%] left-[40%] h-32 md:h-64 lg:h-80 w-8 md:w-16 lg:w-32 border border-neutral-400"/>
-            <div className="absolute top-[20%] right-[20%] h-20 md:h-40 lg:h-60 w-14 md:w-36 lg:w-60 border border-neutral-400"/>
-            <div className="absolute top-[10%] right-[10%] h-20 md:h-40 lg:h-60 w-14 md:w-32 lg:w-48 border border-neutral-400 rounded-full"/>
+            <div className="absolute top-[30%] h-10 md:h-20 lg:h-36 w-20 md:w-40 lg:w-60 border-2 border-neutral-400"/>
+            <div className="absolute top-[20%] left-[40%] h-20 md:h-40 lg:h-60 w-20 md:w-40 lg:w-60 rounded-full border-2 border-neutral-400"/>
+            <div className="absolute top-[20%] left-[40%] h-32 md:h-64 lg:h-80 w-8 md:w-16 lg:w-32 border-2 border-neutral-400"/>
+            <div className="absolute top-[20%] right-[20%] h-20 md:h-40 lg:h-60 w-14 md:w-36 lg:w-60 border-2 border-neutral-400"/>
+            <div className="absolute top-[10%] right-[10%] h-20 md:h-40 lg:h-60 w-14 md:w-32 lg:w-48 border-2 border-neutral-400 rounded-full"/>
           <Image src={canvas_0_ss} alt="product-image" />
         </div>
       </div>
--- apps/frontend/sections/Services.tsx
+++ apps/frontend/sections/Services.tsx
@@ -13,7 +13,7 @@ const Services = () => {
         <div>
           <div className="relative overflow-hidden">
             <div className="border border-neutral-500 h-7 [mask-image:linear-gradient(to_bottom,black,transparent_80%)] rounded-full" />
-            <div className="absolute -top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2 bg-[linear-gradient(to_bottom,rgba(193,52,57,0.3),rgba(193,52,57,0.8))] border h-60 w-60  blur-3xl " />
+            <div className="absolute -top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2 bg-[linear-gradient(to_bottom,rgba(193,52,57,0.3),rgba(252,160,163,0.8))] border h-60 w-60  blur-3xl " />
             <p className="section-description-short mt-4">
               Trusted by 1000+ users, backed by these technologies that never let you down.
             </p>
